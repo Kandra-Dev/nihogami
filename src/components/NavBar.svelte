a173d7e5f067919e8eca4bc53b22137708d444dd
Fabio Ardus <fabio.ardus@gmail.com>
2021-12-29 14:33:22 -0300

    try fixing logo

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -3,10 +3,11 @@
 
 <div class="w-full h-50 fixed bg-dark-beige shadow-md flex justify-between top-0">
   <a href="/">
-    <img alt="Nihogami Logo" class="max-h-full ml-[10px]" src="static/logo.png" />
+    <img alt="Nihogami Logo" class="max-h-full ml-[10px]" src="/logo.png" />
   </a>
   <div class="mr-[10px] flex items-center justify-between align-middle">
     <a class="text-base mx-2 hover:underline hover:font-bold hover:text-dark-red" href="/">Home</a>
-    <a class="text-base mx-2 hover:underline hover:font-bold hover:text-dark-red" href="/cardGame">Vocab Cards</a>
+    <a class="text-base mx-2 hover:underline hover:font-bold hover:text-dark-red" href="/cardGame">Vocabulario</a>
+    <!-- <a class="text-base mx-2 hover:underline hover:font-bold hover:text-dark-red" href="/verbGame">Verbos</a> -->
   </div>
 </div>
